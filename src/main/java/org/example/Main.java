8a14d2598a93aef921467137e2cd68d6924d7f44
ryu-f67 <161839926+ryu-f67@users.noreply.github.com>
2024-08-17 11:55:42 +0900

    携帯電話番号の有効性を確認するプログラムを作成

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    MobilePhoneNumberChecker MPNC = new MobilePhoneNumberChecker();
+    MPNC.checkMobilePhoneNumber();
   }
 }
